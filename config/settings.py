f3192dc6d27a25e2b26cb3b777828e99e6f5dbad
Kevin Bowen <kevin.bowen@gmail.com>
2022-04-20 16:55:24 -0700

    Move base settings to standard dir

--- config/settings.py
+++ config/settings.py
@@ -55,7 +55,7 @@ MIDDLEWARE = [
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
 ]
 
-ROOT_URLCONF = "learning_log.urls"
+ROOT_URLCONF = "config.urls"
 
 TEMPLATES = [
     {
@@ -73,7 +73,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = "learning_log.wsgi.application"
+WSGI_APPLICATION = "config.wsgi.application"
 
 
 # Database

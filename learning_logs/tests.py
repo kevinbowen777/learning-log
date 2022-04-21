96b7e013dc72dfd9e4e4f0e6857b83c1dc5b72be
Kevin Bowen <kevin.bowen@gmail.com>
2022-04-20 18:40:15 -0700

    Write initial tests

--- learning_logs/tests.py
+++ learning_logs/tests.py
@@ -1,3 +1,23 @@
-from django.test import TestCase  # noqa: F401
+from django.test import SimpleTestCase
 
-# Create your tests here.
+
+class LearninLogsTests(SimpleTestCase):
+    def test_homepage_status_code(self):
+        response = self.client.get("/")
+        self.assertEqual(response.status_code, 200)
+
+    def test_topics_page_status_code(self):
+        response = self.client.get("/topics/")
+        self.assertEqual(response.status_code, 302)
+
+    def test_login_page_status(self):
+        response = self.client.get("/users/login/")
+        self.assertEqual(response.status_code, 200)
+
+    def test_logout_page_status(self):
+        response = self.client.get("/users/logout/")
+        self.assertEqual(response.status_code, 200)
+
+    def test_register_page_status(self):
+        response = self.client.get("/users/register/")
+        self.assertEqual(response.status_code, 200)

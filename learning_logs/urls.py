e956a801cf833aafe0c79a79f4eea37f3d5ccaf0
Kevin Bowen <kevin.bowen@gmail.com>
2022-06-08 14:19:01 -0700

    Move about URLs tests to pages

--- learning_logs/urls.py
+++ learning_logs/urls.py
@@ -4,7 +4,6 @@ from . import views
 
 app_name = "learning_logs"
 urlpatterns = [
-    path("about/", views.about, name="about"),
     path("topics/", views.topics, name="topics"),
     path("topics/<int:topic_id>/", views.topic, name="topic"),
     path("new_topic/", views.new_topic, name="new_topic"),
--- pages/tests.py
+++ pages/tests.py
@@ -1,18 +1,16 @@
-from django.test import TestCase
-from django.urls import reverse
+from django.test import SimpleTestCase
+from django.urls import resolve, reverse
+
+from .views import AboutPageView, HomePageView
 
 
-class HomePageTests(TestCase):
+class HomePageTests(SimpleTestCase):
     def setUp(self):
         url = reverse("home")
         self.response = self.client.get(url)
 
     def test_homepage_status_code(self):
         self.assertEqual(self.response.status_code, 200)
-
-    def test_homepage_url_name(self):
-        response = self.client.get(reverse("home"))
-        self.assertEqual(response.status_code, 200)
 
     def test_homepage_template(self):
         self.assertTemplateUsed(self.response, "home.html")
@@ -23,4 +21,38 @@ class HomePageTests(TestCase):
     def test_homepage_does_not_contain_incorrect_html(self):
         self.assertNotContains(
             self.response, "Hi there! I should not be on the page."
+        )
+
+    def test_homepage_url_resolves_homepageview(self):
+        view = resolve("/")
+        self.assertEqual(
+            view.func.__name__,
+            HomePageView.as_view().__name__,
+        )
+
+
+class AboutPageTests(SimpleTestCase):
+    def setUp(self):
+        url = reverse("about")
+        self.response = self.client.get(url)
+
+    def test_aboutpage_status_code(self):
+        self.assertEqual(self.response.status_code, 200)
+
+    def test_aboutpage_template(self):
+        self.assertTemplateUsed(self.response, "about.html")
+
+    def test_aboutpage_homepage_contains_correct_html(self):
+        self.assertContains(self.response, "About page")
+
+    def test_aboutpage_does_not_contain_incorrect_html(self):
+        self.assertNotContains(
+            self.response, "Hi there! I should not be on the page."
+        )
+
+    def test_aboutpage_url_resolves_aboutpageview(self):
+        view = resolve("/about/")
+        self.assertEqual(
+            view.func.__name__,
+            AboutPageView.as_view().__name__,
         )

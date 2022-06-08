5a7253fd38580766dc903b81b68af0ca1fe12fbe
Kevin Bowen <kevin.bowen@gmail.com>
2022-06-08 13:32:58 -0700

    Create home page tests

--- learning_logs/tests.py
+++ learning_logs/tests.py
@@ -2,10 +2,6 @@ from django.test import TestCase
 
 
 class LearningLogsTests(TestCase):
-    def test_homepage_status_code(self):
-        response = self.client.get("/")
-        self.assertEqual(response.status_code, 200)
-
     def test_topics_page_status_code(self):
         response = self.client.get("/topics/")
         self.assertEqual(response.status_code, 302)
--- pages/tests.py
+++ pages/tests.py
@@ -1,3 +1,12 @@
-from django.test import TestCase  # noqa:F401
+from django.test import TestCase
+from django.urls import reverse
 
-# Create your tests here.
+
+class HomePageTests(TestCase):
+    def test_homepage_status_code(self):
+        response = self.client.get("/")
+        self.assertEqual(response.status_code, 200)
+
+    def test_homepage_url_name(self):
+        response = self.client.get(reverse("home"))
+        self.assertEqual(response.status_code, 200)

16235c8f91d711c11b61612f92a1b3a0c9767e50
Kevin Bowen <kevin.bowen@gmail.com>
2022-06-08 13:53:19 -0700

    Test home page HTML

--- pages/tests.py
+++ pages/tests.py
@@ -14,3 +14,13 @@ class HomePageTests(TestCase):
     def test_homepage_template(self):
         response = self.client.get("/")
         self.assertTemplateUsed(response, "home.html")
+
+    def test_homepage_contains_correct_html(self):
+        response = self.client.get("/")
+        self.assertContains(response, "Learning Log")
+
+    def test_homepage_does_not_contain_incorrect_html(self):
+        response = self.client.get("/")
+        self.assertNotContains(
+            response, "Hi there! I should not be on the page."
+        )

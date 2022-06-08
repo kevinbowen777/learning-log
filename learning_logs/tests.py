446280ba09616a4849f9c84978e4f07dc69f7ce3
Kevin Bowen <kevin.bowen@gmail.com>
2022-06-08 13:46:44 -0700

    Remove old tests

--- learning_logs/tests.py
+++ learning_logs/tests.py
@@ -6,6 +6,10 @@ class LearningLogsTests(TestCase):
         response = self.client.get("/topics/")
         self.assertEqual(response.status_code, 302)
 
+    def test_new_topic_page_status_code(self):
+        response = self.client.get("/new_topic/")
+        self.assertEqual(response.status_code, 302)
+
     def test_login_page_status(self):
         response = self.client.get("/accounts/login/")
         self.assertEqual(response.status_code, 200)
@@ -14,6 +18,6 @@ class LearningLogsTests(TestCase):
         response = self.client.get("/accounts/logout/")
         self.assertEqual(response.status_code, 302)
 
-    def test_register_page_status(self):
+    def test_signup_page_status(self):
         response = self.client.get("/accounts/signup/")
         self.assertEqual(response.status_code, 200)

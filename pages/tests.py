f89dc2e87ef746a588d98c76bf4dc53795517a80
Kevin Bowen <kevin.bowen@gmail.com>
2022-06-08 13:58:21 -0700

    Implement setUp method

--- pages/tests.py
+++ pages/tests.py
@@ -3,24 +3,24 @@ from django.urls import reverse
 
 
 class HomePageTests(TestCase):
+    def setUp(self):
+        url = reverse("home")
+        self.response = self.client.get(url)
+
     def test_homepage_status_code(self):
-        response = self.client.get("/")
-        self.assertEqual(response.status_code, 200)
+        self.assertEqual(self.response.status_code, 200)
 
     def test_homepage_url_name(self):
         response = self.client.get(reverse("home"))
         self.assertEqual(response.status_code, 200)
 
     def test_homepage_template(self):
-        response = self.client.get("/")
-        self.assertTemplateUsed(response, "home.html")
+        self.assertTemplateUsed(self.response, "home.html")
 
     def test_homepage_contains_correct_html(self):
-        response = self.client.get("/")
-        self.assertContains(response, "Learning Log")
+        self.assertContains(self.response, "Learning Log")
 
     def test_homepage_does_not_contain_incorrect_html(self):
-        response = self.client.get("/")
         self.assertNotContains(
-            response, "Hi there! I should not be on the page."
+            self.response, "Hi there! I should not be on the page."
         )

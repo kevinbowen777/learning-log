cc5a526841b3842cef7207305f313dc83434eb9f
Kevin Bowen <kevin.bowen@gmail.com>
2022-07-05 20:00:43 -0700

    Move accounts tests to separate modules. Fixes #40

---
+++ accounts/tests/test_models.py
@@ -0,0 +1,68 @@
+from django.contrib.auth import get_user_model
+from django.test import TestCase
+
+
+class CustomUserTests(TestCase):
+    def setUp(self):
+        User = get_user_model()
+        self.user = User.objects.create_user(
+            username="kevin",
+            email="kevin@example.com",
+            password="T3stP@5s123",
+        )
+
+        self.super_user = User.objects.create_superuser(
+            username="superadmin",
+            email="superadmin@example.com",
+            password="T3stP@5s123",
+        )
+
+    def test___str__(self):
+        assert self.user.__str__() == self.user.username
+        assert str(self.user) == self.user.username
+
+    def test_user_get_absolute_url(self):
+        assert (
+            self.user.get_absolute_url() == f"/accounts/{self.user.username}/"
+        )
+
+    def test_create_user(self):
+        self.assertEqual(self.user.username, "kevin")
+        self.assertEqual(self.user.email, "kevin@example.com")
+        self.assertTrue(self.user.is_active)
+        self.assertFalse(self.user.is_staff)
+        self.assertFalse(self.user.is_superuser)
+
+    def test_user_asserts(self):
+        User = get_user_model()
+        try:
+            self.assertIsNotNone(self.user.username)
+        except AttributeError:
+            pass
+        with self.assertRaises(TypeError):
+            User.objects.create_user()
+        with self.assertRaises(TypeError):
+            User.objects.create_user(email="")
+        with self.assertRaises(ValueError):
+            User.objects.create_user(username="", email="", password="foo")
+
+    def test_create_superuser(self):
+        self.assertEqual(self.super_user.username, "superadmin")
+        self.assertEqual(self.super_user.email, "superadmin@example.com")
+        self.assertTrue(self.super_user.is_active)
+        self.assertTrue(self.super_user.is_staff)
+        self.assertTrue(self.super_user.is_superuser)
+
+    def test_superuser_asserts(self):
+        User = get_user_model()
+        try:
+            self.assertIsNotNone(self.super_user.username)
+        except AttributeError:
+            pass
+        with self.assertRaises(ValueError):
+            User.objects.create_superuser(
+                username="",
+                email="super@user.com",
+                password="foo",
+                is_superuser=False,
+            )
--- accounts/tests/test_pages.py
+++ accounts/tests/test_pages.py
@@ -2,36 +2,8 @@ from django.contrib.auth import get_user_model
 from django.test import TestCase
 from django.urls import resolve, reverse
 
-from .forms import CustomUserCreationForm  # noqa:F401
-from .views import SignupPageView
-
-
-class CustomUserTests(TestCase):
-    def test_create_user(self):
-        User = get_user_model()
-        user = User.objects.create_user(
-            username="kevin",
-            email="kevin@example.com",
-            password="T3stP@5s123",
-        )
-        self.assertEqual(user.username, "kevin")
-        self.assertEqual(user.email, "kevin@example.com")
-        self.assertTrue(user.is_active)
-        self.assertFalse(user.is_staff)
-        self.assertFalse(user.is_superuser)
-
-    def test_create_superuser(self):
-        User = get_user_model()
-        user = User.objects.create_superuser(
-            username="superadmin",
-            email="superadmin@example.com",
-            password="T3stP@5s123",
-        )
-        self.assertEqual(user.username, "superadmin")
-        self.assertEqual(user.email, "superadmin@example.com")
-        self.assertTrue(user.is_active)
-        self.assertTrue(user.is_staff)
-        self.assertTrue(user.is_superuser)
+# from ..forms import CustomUserCreationForm  # noqa:F401
+from ..views import SignupPageView
 
 
 class SignupPageTests(TestCase):

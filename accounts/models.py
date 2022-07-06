8a4b6d517708dd89f3a6240f89cf7cf6a68525ac
Kevin Bowen <kevin.bowen@gmail.com>
2022-07-05 18:44:13 -0700

    Add profile_pic & country to CustomUser model

--- accounts/models.py
+++ accounts/models.py
@@ -1,6 +1,7 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 from django.urls import reverse
+from django_countries.fields import CountryField
 
 
 class CustomUser(AbstractUser):
@@ -12,6 +13,8 @@ class CustomUser(AbstractUser):
     name = models.CharField("Name of User", blank=True, max_length=255)
     age = models.PositiveIntegerField(null=True, blank=True)
     bio = models.TextField("Bio", blank=True)
+    profile_pic = models.ImageField(upload_to="profile_pics/", blank=True)
+    country = CountryField("Country", blank=True)
 
     def __str__(self):
         return self.username

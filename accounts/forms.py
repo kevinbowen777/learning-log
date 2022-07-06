a8aba39ae2ddae27a0e0fcbc85aace3cd6898739
Kevin Bowen <kevin.bowen@gmail.com>
2022-07-05 18:46:17 -0700

    Create CustomUser form with duplicate detection

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,14 +1,8 @@
 from django.contrib.auth import get_user_model
 from django.contrib.auth.forms import UserChangeForm, UserCreationForm
+from django.core.exceptions import ValidationError
 
-
-class CustomUserCreationForm(UserCreationForm):
-    class Meta:
-        model = get_user_model()
-        fields = (
-            "email",
-            "username",
-        )
+User = get_user_model
 
 
 class CustomUserChangeForm(UserChangeForm):
@@ -18,3 +12,26 @@ class CustomUserChangeForm(UserChangeForm):
             "email",
             "username",
         )
+
+
+class CustomUserCreationForm(UserCreationForm):
+    error_message = UserCreationForm.error_messages.update(
+        {"duplicate_username": "This username has already been taken."}
+    )
+
+    class Meta:
+        model = get_user_model()
+        fields = (
+            "email",
+            "username",
+        )
+
+    def clean_username(self):
+        username = self.cleaned_data["username"]
+
+        try:
+            User.objects.get(username=username)
+        except User.DoesNotExist:
+            return username
+
+        raise ValidationError(self.error_messages["duplicate_username"])

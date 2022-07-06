e9b869ddbce19bedfa7c1db501d91ce1778b8606
Kevin Bowen <kevin.bowen@gmail.com>
2022-07-05 17:55:40 -0700

    Create CustomUser. Fixes #46

--- accounts/models.py
+++ accounts/models.py
@@ -1,6 +1,26 @@
 from django.contrib.auth.models import AbstractUser
-from django.db import models  # noqa:F401
+from django.db import models
+from django.urls import reverse
 
 
 class CustomUser(AbstractUser):
-    pass
+    """Default custom user model for django-start template project.
+    If adding fields that need to be filled at user signup,
+    check forms.SignupForm and forms.SocialSignupForms accordingly.
+    """
+
+    name = models.CharField("Name of User", blank=True, max_length=255)
+    age = models.PositiveIntegerField(null=True, blank=True)
+    bio = models.TextField("Bio", blank=True)
+
+    def __str__(self):
+        return self.username
+
+    def get_absolute_url(self):
+        """Get URL for user's detail view.
+
+        Returns:
+            str: URL for user detail.
+
+        """
+        return reverse("user_detail", kwargs={"username": self.username})

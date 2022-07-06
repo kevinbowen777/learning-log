0dc9f465b622907e5bb8fd93ec0e9823bc58e405
Kevin Bowen <kevin.bowen@gmail.com>
2022-07-05 19:19:43 -0700

    Fixed missing user_model function

--- accounts/forms.py
+++ accounts/forms.py
@@ -2,12 +2,12 @@ from django.contrib.auth import get_user_model
 from django.contrib.auth.forms import UserChangeForm, UserCreationForm
 from django.core.exceptions import ValidationError
 
-User = get_user_model
+User = get_user_model()
 
 
 class CustomUserChangeForm(UserChangeForm):
     class Meta:
-        model = get_user_model()
+        model = User
         fields = (
             "email",
             "username",
@@ -20,7 +20,7 @@ class CustomUserCreationForm(UserCreationForm):
     )
 
     class Meta:
-        model = get_user_model()
+        model = User
         fields = (
             "email",
             "username",

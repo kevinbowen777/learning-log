53bf58c4e6e50e160f68f8925caa5bbc407e3dd8
Kevin Bowen <kevin.bowen@gmail.com>
2022-07-05 20:35:29 -0700

    Create UserUpdate & Redirect view tests

---
+++ accounts/tests/test_views.py
@@ -0,0 +1,85 @@
+import pytest
+from django.contrib.messages.middleware import MessageMiddleware  # noqa:F401
+from django.contrib.sessions.middleware import SessionMiddleware  # noqa:F401
+from django.http import HttpRequest
+from django.test import RequestFactory
+from django.urls import reverse  # noqa:F401
+
+from ..forms import CustomUserChangeForm
+from ..models import CustomUser
+from ..views import UserRedirectView, UserUpdateView
+
+pytestmark = pytest.mark.django_db
+
+
+class TestUserUpdateView:
+    """TODO:
+
+    extracting view initialization code as class-scoped fixture
+    would be great if only pytest-django supported non-function-scoped
+    fixture db access -- this is a work-in-progress for now:
+    https://github.com/pytest-dev/pytest-django/pull/258
+    """
+
+    def dummy_get_response(self, request: HttpRequest):
+        return None
+
+    def test_get_success_url(
+        self, user: CustomUser, request_factory: RequestFactory
+    ):
+        view = UserUpdateView()
+        request = request_factory.get("/fake-url/")
+        request.user = user
+
+        view.request = request
+
+        assert view.get_success_url() == f"/accounts/{user.username}/"
+
+    def test_get_object(
+        self, user: CustomUser, request_factory: RequestFactory
+    ):
+        view = UserUpdateView()
+        request = request_factory.get("/fake-url/")
+        request.user = user
+
+        view.request = request
+
+        assert view.get_object() == user
+
+    def test_form_valid(
+        self, user: CustomUser, request_factory: RequestFactory
+    ):
+        view = UserUpdateView()
+        form_data = {"name": "John Doe"}
+        request = request_factory.post(reverse("user_update"), form_data)
+
+        # Add the session/message middleware to the request
+        SessionMiddleware(self.dummy_get_response).process_request(request)
+        MessageMiddleware(self.dummy_get_response).process_request(request)
+        request.user = user
+
+        view.request = request
+
+        # Initialize the form
+        form = CustomUserChangeForm()
+        form.cleaned_data = {}
+        view.form_valid(form)
+
+        response = UserUpdateView.as_view()(request)
+        user.refresh_from_db()
+
+        assert response.status_code == 302
+        assert user.name == form_data["name"]
+
+
+class TestUserRedirectView:
+    def test_get_redirect_url(
+        self, user: CustomUser, request_factory: RequestFactory
+    ):
+        view = UserRedirectView()
+        request = request_factory.get("/fake-url")
+        request.user = user
+
+        view.request = request
+
+        assert view.get_redirect_url() == f"/accounts/{user.username}/"

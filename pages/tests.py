6e983f05e1a2d566906e1c07d587fa0b6ad33629
Kevin Bowen <kevin.bowen@gmail.com>
2022-08-25 09:58:10 -0700

    Move home & about to pages dir. Fixes #77

--- pages/tests.py
+++ pages/tests.py
@@ -13,7 +13,7 @@ class HomePageTests(SimpleTestCase):
         self.assertEqual(self.response.status_code, 200)
 
     def test_homepage_template(self):
-        self.assertTemplateUsed(self.response, "home.html")
+        self.assertTemplateUsed(self.response, "pages/home.html")
 
     def test_homepage_contains_correct_html(self):
         self.assertContains(self.response, "Learning Log")
@@ -40,7 +40,7 @@ class AboutPageTests(SimpleTestCase):
         self.assertEqual(self.response.status_code, 200)
 
     def test_aboutpage_template(self):
-        self.assertTemplateUsed(self.response, "about.html")
+        self.assertTemplateUsed(self.response, "pages/about.html")
 
     def test_aboutpage_homepage_contains_correct_html(self):
         self.assertContains(self.response, "About page")
--- pages/views.py
+++ pages/views.py
@@ -2,8 +2,8 @@ from django.views.generic import TemplateView
 
 
 class HomePageView(TemplateView):
-    template_name = "home.html"
+    template_name = "pages/home.html"
 
 
 class AboutPageView(TemplateView):
-    template_name = "about.html"
+    template_name = "pages/about.html"

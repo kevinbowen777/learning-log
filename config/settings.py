d86caa8b6c6f1c4317870dea9c91da8bd824e25a
Kevin Bowen <kevin.bowen@gmail.com>
2022-09-18 16:52:40 -0700

    Add base logging configuration. Resolves #96

--- config/settings.py
+++ config/settings.py
@@ -163,14 +163,51 @@ EMAIL_USE_TLS = True
 MEDIA_URL = "/media/"
 MEDIA_ROOT = str(BASE_DIR.joinpath("media"))
 
-# Heroku settings
-"""
-import os  # noqa: F401, F811
-import django_heroku
-django_heroku.settings(locals())
-"""
+ADMINS = [("Kevin Bowen", "kevinbowen@protonmail.com")]
+MANAGERS = ADMINS
+
+# LOGGING
+LOGGING = {
+    "version": 1,
+    "disable_existing_loggers": False,
+    "filters": {
+        "require_debug_false": {"()": "django.utils.log.RequireDebugFalse"}
+    },
+    "formatters": {
+        "verbose": {
+            "format": "%(levelname)s %(asctime)s %(module)s "
+            "%(process)d %(thread)d %(message)s"
+        }
+    },
+    "handlers": {
+        "mail_admins": {
+            "level": "ERROR",
+            "filters": ["require_debug_false"],
+            "class": "django.utils.log.AdminEmailHandler",
+        },
+        "console": {
+            "level": "DEBUG",
+            "class": "logging.StreamHandler",
+            "formatter": "verbose",
+        },
+    },
+    "root": {"level": "INFO", "handlers": ["console"]},
+    "loggers": {
+        "django.request": {
+            "handlers": ["mail_admins"],
+            "level": "ERROR",
+            "propagate": True,
+        },
+        "django.security.DisallowedHost": {
+            "level": "ERROR",
+            "handlers": ["console", "mail_admins"],
+            "propagate": True,
+        },
+    },
+}
 
 # django-debug-toolbar
+
 # import socket
 # Note: disable when running Nox tests
 # Use the following in Docker only:

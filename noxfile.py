a0b33974cb5e020c81b8543aeae75b41b10c3213
Kevin Bowen <kevin.bowen@gmail.com>
2022-12-01 17:35:28 -0800

    Restore flake8-import-order. Fixes #152

--- noxfile.py
+++ noxfile.py
@@ -68,6 +68,7 @@ def lint(session):
         "flake8-bandit",
         "flake8-black",
         "flake8-bugbear",
+        "flake8-import-order",
         # "flake8-docstrings",
     )
     session.run("flake8", *args)

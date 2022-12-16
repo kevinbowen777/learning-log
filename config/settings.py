9e1e9d70ad60ea412d5e59e8ec1b85d8d63377c3
Kevin Bowen <kevin.bowen@gmail.com>
2022-12-16 14:37:13 -0800

    Remove herokuapp. Fixes #161

--- config/settings.py
+++ config/settings.py
@@ -15,7 +15,7 @@ SECRET_KEY = env.str("SECRET_KEY")
 
 DEBUG = env.bool("DEBUG", default=False)
 
-ALLOWED_HOSTS = [".herokuapp.com", "localhost", "127.0.0.1"]
+ALLOWED_HOSTS = ["localhost", "127.0.0.1"]
 
 
 # Application definitions

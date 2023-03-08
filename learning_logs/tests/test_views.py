d11871a1550fa85d35d1d3f7eca4a8403fe4c7e5
Kevin Bowen <kevin.bowen@gmail.com>
2023-03-07 18:13:17 -0800

    Improved test coverage

--- learning_logs/tests/test_views.py
+++ learning_logs/tests/test_views.py
@@ -6,13 +6,13 @@ from ..models import Entry, Topic  # noqa:F401
 from ..views import topic, topics  # noqa:F401
 
 
-class TopicsPageViewTest(TestCase):
+class TopicPageViewTest(TestCase):
     def setUp(self):
         User = get_user_model()
         self.user = User.objects.create_user(
             username="kevin",
             email="kevin@example.com",
-            password="T3stP@5s123",
+            password="secret",
         )
 
         self.topic = Topic.objects.create(
@@ -21,6 +21,7 @@ class TopicsPageViewTest(TestCase):
         )
 
         self.entry = Topic.objects.create(
+            # topic = self.entry.topic,
             text="Entry text",
             owner=self.user,
         )
@@ -32,3 +33,61 @@ class TopicsPageViewTest(TestCase):
     def test___str__(self):
         assert self.topic.__str__() == self.topic.text
         assert str(self.topic) == self.topic.text
+
+    def test_topic_create_view(self):
+        self.client.login(email="kevin@example.com", password="secret")
+        response = self.client.post(
+            reverse("new_topic"),
+            {
+                "text": "This is a new topic",
+            },
+        )
+        self.assertEqual(response.status_code, 302)
+        self.assertEqual(Topic.objects.last().text, "This is a new topic")
+        self.assertEqual(Topic.objects.first().text, "A Topic title")
+        self.assertTemplateUsed("learning_logs/new_topic.html")
+        # self.assertEqual(Article.objects.last().body, "Nice body content")
+
+    def test_topic_list_single(self):
+        self.client.login(email="kevin@example.com", password="secret")
+        response = self.client.get(
+            reverse("topic", args={self.topic.id}),
+            # reverse("topic"),
+        )
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(Topic.objects.first().text, "A Topic title")
+        self.assertTemplateUsed("learning_logs/topic.html")
+
+    def test_topics_list_page(self):
+        self.client.login(email="kevin@example.com", password="secret")
+        response = self.client.get(
+            # reverse("topics", args={self.topic.id}),
+            reverse("topics"),
+        )
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed("learning_logs/topics.html")
+        # self.assertEqual(Topic.objects.first().text, "A Topic title")
+
+    def test_create_new_entry(self):
+        self.client.login(email="kevin@example.com", password="secret")
+        response = self.client.post(
+            reverse("new_entry", args={self.topic.id}),
+            {
+                "text": "This is a new entry",
+            },
+        )
+        self.assertEqual(response.status_code, 302)
+        self.assertTemplateUsed("learning_logs/new_entry.html")
+
+    """
+    def test_create_new_entry(self):
+        self.client.login(email="kevin@example.com", password="secret")
+        response = self.client.post(
+            reverse("edit_entry", args={self.entry.id}),
+            {
+                "text": "This is an edited entry",
+            },
+        )
+        self.assertEqual(response.status_code, 302)
+        self.assertTemplateUsed("learning_logs/edit_entry.html")
+    """

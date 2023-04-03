e83e58969f686ff3b9ad3c1a0ad6dca8ba1b3d53
Kevin Bowen <kevin.bowen@gmail.com>
2023-04-02 23:25:53 -0700

    Upgrade factory-boy. Replace password function with PostGenerationMethodCall. Resolves #113

--- accounts/tests/factories.py
+++ accounts/tests/factories.py
@@ -1,31 +1,13 @@
-from typing import Any, Sequence
-
 from django.contrib.auth import get_user_model
-from factory import Faker, post_generation
+from factory import Faker, PostGenerationMethodCall
 from factory.django import DjangoModelFactory
 
 
 class UserFactory(DjangoModelFactory):
-
     username = Faker("user_name")
     email = Faker("email")
     name = Faker("name")
-
-    @post_generation
-    def password(self, create: bool, extracted: Sequence[Any], **kwargs):
-        password = (
-            extracted
-            if extracted
-            else Faker(
-                "password",
-                length=42,
-                special_chars=True,
-                digits=True,
-                upper_case=True,
-                lower_case=True,
-            ).generate(extra_kwargs={})
-        )
-        self.set_password(password)
+    password = PostGenerationMethodCall("set_password", "P@s5word")
 
     class Meta:
         model = get_user_model()

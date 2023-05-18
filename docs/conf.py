d30be6663b74ccccbc5b528b296f94ce65f01759
Kevin Bowen <kevin.bowen@gmail.com>
2023-05-18 14:36:03 -0700

    Downgrade Sphinx. Install, and build initial theme config. Resolves #246

--- docs/conf.py
+++ docs/conf.py
@@ -1,4 +1,26 @@
 """Sphinx configuration."""
-project = "learning_log"
+project = "learning-log"
 author = "Kevin Bowen"
 copyright = f"2023, {author}"
+#
+html_theme = "sphinx_rtd_theme"
+html_theme_options = {
+    'analytics_id': 'G-XXXXXXXXXX',  #  Provided by Google in your dashboard
+    'analytics_anonymize_ip': False,
+    'logo_only': False,
+    'display_version': True,
+    'prev_next_buttons_location': 'bottom',
+    'style_external_links': False,
+    'vcs_pageview_mode': '',
+    'style_nav_header_background': 'white',
+    # ToC options
+    'collapse_navigation': True,
+    'sticky_navigation': True,
+    'navigation_depth': 4,
+    'includehidden': True,
+    'titles_only': False
+}
+extensions = [
+    'sphinx.ext.duration',
+    'sphinx_rtd_theme',
+]

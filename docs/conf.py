f3eb9b3acc83b27794a6e1fcf5f9954cc42482af
Kevin Bowen <kevin.bowen@gmail.com>
2023-07-27 00:47:55 -0700

    Install furo theme. Resolves #292

--- docs/conf.py
+++ docs/conf.py
@@ -3,24 +3,9 @@ project = "learning-log"
 author = "Kevin Bowen"
 copyright = f"2023, {author}"
 #
-html_theme = "sphinx_rtd_theme"
-html_theme_options = {
-    'analytics_id': 'G-XXXXXXXXXX',  #  Provided by Google in your dashboard
-    'analytics_anonymize_ip': False,
-    'logo_only': False,
-    'display_version': True,
-    'prev_next_buttons_location': 'bottom',
-    'style_external_links': False,
-    'vcs_pageview_mode': '',
-    'style_nav_header_background': 'white',
-    # ToC options
-    'collapse_navigation': True,
-    'sticky_navigation': True,
-    'navigation_depth': 4,
-    'includehidden': True,
-    'titles_only': False
-}
+html_theme = 'furo'
+html_logo = 'django_24.png'
+html_title = 'learning-log'
 extensions = [
     'sphinx.ext.duration',
-    'sphinx_rtd_theme',
 ]

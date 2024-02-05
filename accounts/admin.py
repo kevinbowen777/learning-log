f26a1e932bb38ec94bafa08c3b9f84c250655282
Kevin Bowen <kevin.bowen@gmail.com>
2024-02-04 18:14:07 -0800

    Update admin fields. Resolves #299

--- accounts/admin.py
+++ accounts/admin.py
@@ -7,6 +7,7 @@ from .forms import CustomUserChangeForm, CustomUserCreationForm
 User = get_user_model()
 
 
+@admin.register(User)
 class CustomUserAdmin(UserAdmin):
     add_form = CustomUserCreationForm
     form = CustomUserChangeForm
@@ -16,13 +17,33 @@ class CustomUserAdmin(UserAdmin):
         "username",
         "is_staff",
     ]
-    fieldsets = UserAdmin.fieldsets
-    fieldsets[1][1]["fields"] = fieldsets[1][1]["fields"] + (
-        "age",
-        "country",
-        "profile_pic",
-        "bio",
+    fieldsets = (
+        (None, {"fields": ("username", "password")}),
+        (
+            "Personal information",
+            {
+                "fields": (
+                    "first_name",
+                    "last_name",
+                    "email",
+                    "age",
+                    "country",
+                    "profile_pic",
+                    "bio",
+                )
+            },
+        ),
+        (
+            "Permissions",
+            {
+                "fields": (
+                    "is_active",
+                    "is_staff",
+                    "is_superuser",
+                    "groups",
+                    "user_permissions",
+                )
+            },
+        ),
+        ("Important dates", {"fields": ("last_login", "date_joined")}),
     )
-
-
-admin.site.register(User, CustomUserAdmin)

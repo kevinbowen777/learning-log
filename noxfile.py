c766ec180d59dd9f1973bb64c16a544ba061ceea
Kevin Bowen <kevin.bowen@gmail.com>
2025-06-19 11:31:18 -0700

    Update nox pip-audit exclusions. Resolves #544 (#546)

--- noxfile.py
+++ noxfile.py
@@ -88,8 +88,8 @@ def audit(session):
         "pip-audit",
         "--desc",
         "--aliases",
-        # "--ignore-vuln",
-        # "PYSEC-2025-14",
+        "--ignore-vuln",
+        "PYSEC-2025-49",
     )
 
 
